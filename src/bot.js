f37cac14732bc982528fc3f5ccf3537c00b58389
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-06-05 12:11:28 +0500

    bismillah

--- src/bot.js
+++ src/bot.js
@@ -3,13 +3,5 @@ const { Telegraf } = require("telegraf");
 
 const bot =new Telegraf(process.env.BOT_TOKEN)
 
-
-
-
-bot.use(ctx => {
-    console.log(ctx);
-    ctx.reply("ctx")
-})
-
  
 module.exports = bot
